15e1f8cfd650b4ba0d2cbbe2d16c7a7c64c924b9
Ezequiel Ramis <ezequielramis.hello@gmail.com>
2021-02-01 18:42:22 -0300

    testing tokens

--- api/tweet.ts
+++ api/tweet.ts
@@ -1,6 +1,15 @@
 import { NowRequest, NowResponse } from "@vercel/node";
+// import { TwitterClient } from "twitter-api-client";
 
-export default (request: NowRequest, response: NowResponse) => {
-  const { name = "World" } = request.query;
-  response.status(200).send(`Hello ${name}!`);
+export default (req: NowRequest, res: NowResponse) => {
+  const { ats, at, b, aks, ak } = req.query;
+  const {
+    ACCESS_TOKEN_SECRET,
+    ACCESS_TOKEN,
+    BEARER,
+    API_KEY_SECRET,
+    API_KEY,
+  } = process.env;
+  const str = `${ats} ${ACCESS_TOKEN_SECRET} \n ${at} ${ACCESS_TOKEN} \n ${b} ${BEARER} \n ${aks} ${API_KEY_SECRET} \n ${ak} ${API_KEY}`;
+  res.send(str);
 };
