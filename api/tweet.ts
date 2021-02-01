4caf9cb33c59f3966b0064e76ecb096907aa9971
Ezequiel Ramis <ezequielramis.hello@gmail.com>
2021-02-01 19:32:04 -0300

    gh action test

--- api/tweet.ts
+++ api/tweet.ts
@@ -1,15 +1,32 @@
 import { NowRequest, NowResponse } from "@vercel/node";
-// import { TwitterClient } from "twitter-api-client";
+import { TwitterClient } from "twitter-api-client";
 
 export default (req: NowRequest, res: NowResponse) => {
-  const { ats, at, b, aks, ak } = req.query;
+  const { ats, at, aks, ak } = req.query;
   const {
     ACCESS_TOKEN_SECRET,
     ACCESS_TOKEN,
-    BEARER,
     API_KEY_SECRET,
     API_KEY,
   } = process.env;
-  const str = `${ats} ${ACCESS_TOKEN_SECRET} \n ${at} ${ACCESS_TOKEN} \n ${b} ${BEARER} \n ${aks} ${API_KEY_SECRET} \n ${ak} ${API_KEY}`;
-  res.send(str);
+  if (
+    ats === ACCESS_TOKEN_SECRET &&
+    at === ACCESS_TOKEN &&
+    aks === API_KEY_SECRET &&
+    ak === API_KEY
+  ) {
+    const t = new TwitterClient({
+      apiKey: ak,
+      apiSecret: aks,
+      accessToken: at,
+      accessTokenSecret: ats,
+    });
+    t.tweets
+      .statusesUpdate({ status: "hello from twitter api" })
+      .then((v) => {
+        res.status(200).send(`${v.created_at}: ${v.text}`);
+      })
+      .catch((err) => res.status(500).send(err));
+  }
+  res.end();
 };
