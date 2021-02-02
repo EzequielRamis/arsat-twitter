5fcebd978879446280bc6229912babdb1dbbba11
Ezequiel Ramis <ezequielramis.hello@gmail.com>
2021-02-01 23:16:20 -0300

    my god, it wasn't async

--- api/tweet.ts
+++ api/tweet.ts
@@ -1,32 +1,32 @@
 import { NowRequest, NowResponse } from "@vercel/node";
 import { TwitterClient } from "twitter-api-client";
 
-export default (req: NowRequest, res: NowResponse) => {
+export default async (req: NowRequest, res: NowResponse) => {
   const { ats, at, aks, ak } = req.query;
-  // const {
-  //   ACCESS_TOKEN_SECRET,
-  //   ACCESS_TOKEN,
-  //   API_KEY_SECRET,
-  //   API_KEY,
-  // } = process.env;
-  // if (
-  //   ats === ACCESS_TOKEN_SECRET &&
-  //   at === ACCESS_TOKEN &&
-  //   aks === API_KEY_SECRET &&
-  //   ak === API_KEY
-  // ) {
-  const t = new TwitterClient({
-    apiKey: ak.toString(),
-    apiSecret: aks.toString(),
-    accessToken: at.toString(),
-    accessTokenSecret: ats.toString(),
-  });
-  t.tweets
-    .statusesUpdate({ status: "hello from twitter api" })
-    .then((v) => {
-      res.status(200).send(`${v.created_at}: ${v.text}`);
-    })
-    .catch((err) => res.status(500).send(err));
-  // }
-  res.end();
+  const {
+    ACCESS_TOKEN_SECRET,
+    ACCESS_TOKEN,
+    API_KEY_SECRET,
+    API_KEY,
+  } = process.env;
+  if (
+    ats === ACCESS_TOKEN_SECRET &&
+    at === ACCESS_TOKEN &&
+    aks === API_KEY_SECRET &&
+    ak === API_KEY
+  ) {
+    const t = new TwitterClient({
+      apiKey: ak,
+      apiSecret: aks,
+      accessToken: at,
+      accessTokenSecret: ats,
+    });
+    t.tweets
+      .statusesUpdate({ status: "hello from twitter api" })
+      .then((v) => {
+        res.status(200).send(`${v.created_at}: ${v.text}`);
+      })
+      .catch((err) => res.status(500).send(err));
+  }
+  res.status(500).end();
 };
